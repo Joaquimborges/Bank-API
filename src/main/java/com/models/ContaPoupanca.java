f42bfef805c73a64a6090a827078ddc458c2c558
joaquimborges <joaquim.borges1993@gmail.com>
2021-09-09 10:55:48 -0300

    excluding unnecessary class

--- src/main/java/com/models/ContaPoupanca.java
+++
@@ -1,8 +0,0 @@
-package com.models;
-
-public class ContaPoupanca extends Conta{
-
-    public ContaPoupanca(String nome, String numero, String agencia, String tipoDeConta, double saldo, boolean especial, double investimento) {
-        super(nome, numero, agencia, tipoDeConta, saldo, especial, investimento);
-    }
-}
